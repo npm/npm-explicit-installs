43cd82e283a32dce865f9e5b52bb631477caac32
Benjamin Coe <ben@npmjs.com>
2016-02-02 11:38:58 -0800

    add support for proxy/non-standard registry URL

---
+++ bin/npm-explicit-installs.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var npmExplicitInstalls = require('../')
+var chalk = require('chalk')
+
+require('yargs')
+  .usage('$0 <cmd> [options]')
+  .command('dry-run', 'show the packages that would be displayed on the home page', function () {
+    npmExplicitInstalls(function (err, pkgs) {
+      pkgs.forEach(function (pkg) {
+        console.log(chalk.green(pkg.name), '(' + pkg.version + ')', chalk.gray(pkg.publisher.name))
+      })
+      npmExplicitInstalls.client.end()
+    })
+  })
+  .command('delete', 'delete packages from the home page', function () {
+  })
+  .demand(1)
+  .argv
