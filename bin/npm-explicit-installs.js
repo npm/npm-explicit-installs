a7ae36cf6af1db7e46e80a8ae0205c5d2f923d32
Benjamin Coe <ben@npmjs.com>
2016-02-02 15:26:52 -0800

    there's now a command-line API for editing the home page packages

--- bin/npm-explicit-installs.js
+++ bin/npm-explicit-installs.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-var npmExplicitInstalls = require('../')
 var chalk = require('chalk')
+var fs = require('fs')
+var path = require('path')
+var inquirer = require('inquirer')
 
 require('yargs')
   .usage('$0 <cmd> [options]')
   .command('dry-run', 'show the packages that would be displayed on the home page', function () {
+    var npmExplicitInstalls = require('../')
     npmExplicitInstalls.client.on('connect', function () {
       npmExplicitInstalls(function (err, pkgs) {
         if (err) {
@@ -21,6 +24,42 @@ require('yargs')
     })
   })
   .command('delete', 'delete packages from the home page', function () {
+    var packages = require('../packages')
+    inquirer.prompt({
+      name: 'package',
+      message: 'remove package from homepage',
+      type: 'list',
+      choices: packages,
+    }, function (answer) {
+      packages.splice(packages.indexOf(answer.package), 1)
+      fs.writeFileSync(path.resolve(__dirname, '../packages.json'), JSON.stringify(packages, null, 2), 'utf-8')
+    })
+  })
+  .command('add', 'add a new package to the home page', function () {
+    var packages = require('../packages')
+    var logos = require('../logos')
+
+    inquirer.prompt([
+      {
+        name: 'package',
+        message: 'name of package to add',
+        validate: function (input) {
+          if (!input.length) return 'you must provide a package name'
+          else return true
+        }
+      },
+      {
+        name: 'logo',
+        message: 'url of icon to use for package (optional)'
+      }
+    ], function (answer) {
+      if (answer.logo) {
+        logos[answer.package] = answer.logo
+        fs.writeFileSync(path.resolve(__dirname, '../logos.json'), JSON.stringify(logos, null, 2), 'utf-8')
+      }
+      packages.push(answer.package)
+      fs.writeFileSync(path.resolve(__dirname, '../packages.json'), JSON.stringify(packages, null, 2), 'utf-8')
+    })
   })
   .help('help')
   .alias('h', 'help')
