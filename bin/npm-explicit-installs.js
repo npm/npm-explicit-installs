38781757fa78ef2055cfa5a0ea268ef13f2a9656
Benjamin Coe <ben@npmjs.com>
2016-02-02 11:44:27 -0800

    wait for redis to connect before we list packages

--- bin/npm-explicit-installs.js
+++ bin/npm-explicit-installs.js
@@ -6,11 +6,13 @@ var chalk = require('chalk')
 require('yargs')
   .usage('$0 <cmd> [options]')
   .command('dry-run', 'show the packages that would be displayed on the home page', function () {
-    npmExplicitInstalls(function (err, pkgs) {
-      pkgs.forEach(function (pkg) {
-        console.log(chalk.green(pkg.name), '(' + pkg.version + ')', chalk.gray(pkg.publisher.name))
+    npmExplicitInstalls.client.on("connect", function () {
+      npmExplicitInstalls(function (err, pkgs) {
+        pkgs.forEach(function (pkg) {
+          console.log(chalk.green(pkg.name), '(' + pkg.version + ')', chalk.gray(pkg.publisher.name))
+        })
+        npmExplicitInstalls.client.end()
       })
-      npmExplicitInstalls.client.end()
     })
   })
   .command('delete', 'delete packages from the home page', function () {
